cf4b802e5d26ab4b3c2a485015d66d68b953e055
zhuominlooper <zm19956251995625>
2021-11-04 14:48:29 +0800

    修改打包名称

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ const config = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: `index.umd${isMinPack ? ".min" : ""}.js`,
-    library: "extend-promise", //打包导出的变量名称
-    globalObject: "this", // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
-    libraryTarget, // 定义打包方式Universal Module Definition,根据环境判断打包方式同时支持在CommonJS、AMD，Script和全局变量使用
+    library: "myPromise", 
+    globalObject: "this", 
+    libraryTarget, 
   },
   module: {
     rules: [
